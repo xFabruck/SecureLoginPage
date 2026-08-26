0e5b0b4116d60c8a6d54af33dec48d8651d2c7bf
xFabruck <149441255+xFabruck@users.noreply.github.com>
2026-08-26 10:41:35 -0500

    Punto 9 realizado completamente
    Protenccion ante intentos consecutivos propuesta por la guia

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,11 @@
 
 namespace App\Providers;
 
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\Str;
+
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -17,8 +22,14 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
+    
     public function boot(): void
     {
-        //
+        RateLimiter::for('login', function (Request $request) {
+            $email = Str::lower((string) $request->input('email'));
+            
+            return Limit::perMinute(5)
+                ->by($email.'|'.$request->ip());
+        });
     }
 }
